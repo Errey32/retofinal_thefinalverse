54f63ea9bf5a26dd8da77f7b8fd5cce828e7a287
Errey32 <127329213+Errey32@users.noreply.github.com>
2023-05-11 00:53:16 -0500

    x
    x

--- characters.py
+++ characters.py
@@ -1,29 +1,30 @@
 from msilib.schema import SelfReg
 from time import sleep
 from os import system
-from stats import fight_system
+from stats import FightSystem, Enemy
 import sys
 
 delay = 1
-print("---------Bienvenido a nuestro mundo--------")
+print("---------Welcome to our world--------")
 sleep(delay)
-input("Presione una tecla para continuar...")
+input("Press a key to continue...")
 
 system('cls')
 
-delay = 4
+delay = 1
 lang = input("----Wich class are you going to choose for your new game?----\n\t\t\t[Human]\n\t\t\t[Demon]\n\t\t\t[Angel]\n-------------------------------------------------------------\n")
+
 print("You just woke up from a hundred years sleep, is a dark place you are trapped somewhere so you push and get out from a coffin. ")
 sleep(delay)
 print("You are in the middle of a snowy forest it is to cold for you soyou start lookin around and you see a cabin and you peek in and see some Hunters.")
 sleep(delay)
 lang2 = input("What are you going to do?\n\t\t\t[1.- You go fight the Hunters]\n\t\t\t[2.- You go searchin for another place to shellter]")
 if lang2 == "1":
-        fight_system
+        FightSystem(lang).start(Enemy('hunter'))
         system("cls")
         print("You have defeated the hunters, you stay inside the shellter a few hours to sleep")
         sleep(delay)
-                        
+
 elif lang2 == "2":
     print("You found another cabain so you stay a few hours to sleep")
 
@@ -31,7 +32,8 @@ print("YOU WOKE UP, something loud woke you, you start lookin around in the midd
 sleep(delay)
 print("You find some big guy comming for you, he is covered in flames, you need to fight him.")
 sleep(delay)
-#pelea
+FightSystem(lang).start(Enemy('flameguy'))
+system("cls")
 print("You destroyed that guy so you run to see his things and you find the godslayer sword")
 sleep(delay)
 print("With this sword you can defeat the elden ghost that is in the deep of the forest")
@@ -41,7 +43,7 @@ print("These angels aren't what they seem to be")
 sleep(delay)
 print("He starts attackin you but you manage to dodge his attacks")
 sleep(delay)
-#pelea
+FightSystem(lang).start(Enemy('fallenangel'))
 system("cls")
 print("The way this angel acts is like if he wasn't in control")
 sleep(delay)
@@ -56,8 +58,8 @@ print("in this book you found everything from the elden ghost and everything tha
 sleep(delay)
 print("You need to fight him and defeat him, he does not deserve to live")
 sleep(delay)
-print("You got out of the house and some squeletons are waiting for you, Defeat them")
-#pelea
+print("You got out of the house and some skeletons are waiting for you, Defeat them")
+FightSystem(lang).start(Enemy('skeletons'))
 system("cls")
 print("You are super tired of fighting all day, you need to start lookin for a place to sleep")
 print("You start lookin for a cabin, you walk for some time so yo decide to start a fire and sleep outside")
@@ -73,13 +75,13 @@ sleep(delay)
 print("You walk deeper into the cave, and you find some goblins takin the armour from the bodies")
 print("They saw you so you fight them")
 sleep(delay)
-#Fight         
+FightSystem(lang).start(Enemy('goblin'))       
 system("cls")
 print("You saw that a lot of enemies are running to you so you prepared to fight all of them")
 print("But they run pass you, if like they were running from someone")
 print("You hear the silbing noise, its a scary whithe silbing that horrifies you.")
 print("You need to fight that thing")
-#fight
+FightSystem(lang).start(Enemy('eldenghost'))
 system("cls")
 print("You have defeated the elden ghost you are now free")
 print("End of the Game")
--- stats.py
+++ stats.py
@@ -1,6 +1,4 @@
-from time import sleep
-from os import system
-import random
+from random import randint, choice
 
 #Aqui van a estar las bases de stats de las razas
 class Individual:
@@ -35,277 +33,241 @@ class Individual:
 
     def __init__(self, **options) -> None:
         self.hp: int = options.get("hp", None) or 150 
-        self.Defense: int = options.get("defense", None) or 50
+        self.defense: int = options.get("defense", None) or 50
         self.mana: int = options.get("mana", None) or 50
         self.faith: int = options.get("faith", None) or 50
-        self.dark: int = options.get("darkness", None) or 50
-        self.strenght: int = options.get("strenght", None) or 150
+        self.dark: int = options.get("dark", None) or 50
+        self.strength: int = options.get("strength", None) or 150
 
     def __repr__(self) -> str:
-        return f"Hp: {self.hp}\n Defense: {self.Defense}\n Mana: {self.mana}\n Faith: {self.faith}\n Darkness: { self.dark}\n Strenght: {self.strenght}\n"
-    
-    def attack (self, target):
-        target.hp -= self.Defense
+        return f"Hp: {self.hp}\nDefense: {self.defense}\nMana: {self.mana}\nFaith: {self.faith}\nDarkness: {self.dark}\nStrength: {self.strength}\n"
 
-    def attack(self, target):
-        damage = self.attack - target.Defense
-        if damage < 0 - target.Defense:
-            damage = 0
-        target.recib_damage(damage)
-        
-    def recib_damage(self, damage):
+
+    def receive_damage(self, damage):
         self.hp -= damage
-        print(f"recib {damage} of damage.")
+        print(f"Received {damage} damage.")
         if self.hp <= 0:
-            print(f"You are Dead")
-            
-    def curar(self, amount):
+            print("You are dead.")
+
+    def heal(self):
+        amount = choice([10, 30, 50, 70, 90, 110])
         self.hp += amount
-        if self.hp > 100:
-            self.hp = 100
-        print(f"has been cured {amount} life points.")
-        
-    def defenderse(self):
-        self.defensa = 5
-        print(f"¡it's defending!.")
-        
-    def reset_defensa(self):
-        self.defensa = 0
+        print(f"{self} has been healed for {amount} HP.")
+
+    def defend(self):
+        self.defense += 5
+        print("You are defending.")
+
+    def reset_defense(self):
+        self.defense = 0
 
 class Human(Individual):
     def __init__(self, **options) -> None:
-        super().__init__(**super().base_stats['human'])
-    
+        super().__init__(**Individual.base_stats['human'])
+
     def attack(self, target):
-        target.hp -= self.strenght
+        damage = self.strength - target.defense
+        if damage < 0:
+            damage = 0
+        target.receive_damage(damage)
 
 class Demon(Individual):
     def __init__(self, **options) -> None:
-        super().__init__(**super().base_stats['demon'])
+        super().__init__(**Individual.base_stats['demon'])
 
-    def attack (self, target):
-        target.hp -= self.dark
-      
+    def attack(self, target):
+        damage = self.dark - target.defense
+        if damage < 0:
+            damage = 0
+        target.receive_damage(damage)
+
 class Angel(Individual):
     def __init__(self, **options) -> None:
-        super().__init__(**super().base_stats['angel'])
-    
-    def attack (self, target):
-        target.hp -= self.faith
+        super().__init__(**Individual.base_stats['angel'])
 
-#Stats de los enemigos y boses
+    def attack(self, target):
+        damage = self.faith - target.defense
+        if damage < 0:
+            damage = 0
+        target.receive_damage(damage)
 
-class enemy_stats:
-
-    enemy = {
-
+class Enemy:
+    enemy_stats = {
         'hunter': {
-            'hp': 110, 
+            'hp': 110,
             'defense': 10,
             'mana': 30,
             'faith': 20,
             'dark': 20,
-            'strenght': 50,
-        },
-        'thieve': {
-            'hp': 120, 
-            'defense': 15,
-            'mana': 90,
-            'faith': 30,
-            'dark': 30,
-            'strenght': 60,
+            'strength': 50,
         },
         'goblin': {
-            'hp': 70, 
+            'hp': 70,
             'defense': 5,
             'mana': 30,
             'faith': 0,
             'dark': 40,
-            'strenght': 30,
+            'strength': 30,
         },
-        'knight':{
-            'hp': 140, 
-            'defense': 50,
-            'mana': 80,
-            'faith': 80,
-            'dark': 40,
-            'strenght': 100,
-        },
-        'dark knigh':{
-            'hp': 180, 
-            'defense': 100,
-            'mana': 140,
-            'faith': 120,
-            'dark': 80,
-            'strenght': 160,
-        },
-        ' boss holy knight':{
-            'hp': 250, 
-            'defense': 180,
-            'mana': 200,
-            'faith': 180,
-            'dark': 120,
-            'strenght': 200,
-        }, 
-        'esqueletons':{
-            'hp': 180, 
+        'fallenangel': {
+            'hp': 190,
             'defense': 120,
             'mana': 100,
             'faith': 0,
             'dark': 110,
-            'strenght': 80,
+            'strength': 100,
         },
-        'malenia':{
-            'hp': 300, 
-            'defense': 200,
-            'mana': 190,
-            'faith': 160,
-            'dark': 60,
-            'strenght': 230,
+        'flameguy': {
+            'hp': 190,
+            'defense': 120,
+            'mana': 100,
+            'faith': 0,
+            'dark': 110,
+            'strength': 100,
         },
-        'elden ghost':{
-            'hp': 350, 
+        'skeletons': {
+            'hp': 180,
+            'defense': 120,
+            'mana': 100,
+            'faith': 0,
+            'dark': 110,
+            'strength': 80,
+        },
+        'elden ghost': {
+            'hp': 350,
             'defense': 240,
             'mana': 230,
             'faith': 300,
             'dark': 0,
-            'strenght': 200,
-        },
-        'Demon King':{
-            'hp': 350, 
-            'defense': 240,
-            'mana': 230,
-            'faith': 0,
-            'dark': 300,
-            'strenght': 200,
+            'strength': 200,
         }
     }
 
-    def __init__(self, **options) -> None:
-        self.hp: int = options.get("hp", None) or 150 
-        self.Defense: int = options.get("defense", None) or 50
-        self.mana: int = options.get("mana", None) or 50
-        self.faith: int = options.get("faith", None) or 50
-        self.dark: int = options.get("darkness", None) or 50
-        self.strenght: int = options.get("strenght", None) or 150
+    def __init__(self, enemy_type):
+        stats = self.enemy_stats.get(enemy_type)
+        if stats is not None:
+            self.enemy_type = enemy_type.title()
+            self.hp = stats.get('hp', 150)
+            self.defense = stats.get('defense', 50)
+            self.mana = stats.get('mana', 50)
+            self.faith = stats.get('faith', 50)
+            self.dark = stats.get('dark', 50)
+            self.strength = stats.get('strength', 150)
+        else:
+            raise ValueError(f"Invalid enemy type: {enemy_type}")
 
     def __repr__(self) -> str:
-        return f"Hp: {self.hp}\n Defense: {self.Defense}\n Mana: {self.mana}\n Faith: {self.faith}\n Darkness: { self.dark}\n Strenght: {self.strenght}\n"
-    
-    def attack (self, target):
-        target.hp -= self.Defense
+        return f"Hp: {self.hp}\nDefense: {self.defense}\nMana: {self.mana}\nFaith: {self.faith}\nDarkness: {self.dark}\nStrength: {self.strength}\n"
 
     def attack(self, target):
-        damage = self.attack - target.Defense
-        if damage < 0 - target.Defense:
+        damage = self.strength - target.defense
+        if damage < 0:
             damage = 0
-        target.recib_damage(damage)
-        
-    def recib_damage(self, damage):
+        target.receive_damage(damage)
+
+    def receive_damage(self, damage):
         self.hp -= damage
-        print(f"recib {damage} of damage.")
+        print(f"Received {damage} damage.")
         if self.hp <= 0:
-            print(f"You are Dead")
-            
-    def curar(self, amount):
-        self.hp += amount
-        if self.hp > 100:
-            self.hp = 100
-        print(f"has been cured {amount} life points.")
-        
-    def defenderse(self):
-        self.defensa = 5
-        print(f"¡it's defending!.")
-        
-    def reset_defensa(self):
-        self.defensa = 0
+            print("The enemy is defeated.")
 
-class hunter(enemy_stats):
-    def __init__(self, **options) -> None:
-        super().__init__(**super().enemy['hunter'])
+    def heal(self, target):
+        amount = randint(10, 30)
+        target.heal(amount)
+        print(f"{target} has been healed for {amount} HP.")
 
-class thieve(enemy_stats):
-    def __init__(self, **options) -> None:
-        super().__init__(**super().enemy['thieve'])
+    def defend(self):
+        self.defense += 5
+        print("Defending!")
 
-class goblin(enemy_stats):
-    def __init__(self, **options) -> None:
-        super().__init__(**super().enemy['goblin'])
+    def reset_defense(self):
+        self.defense = 0
 
-class knight(enemy_stats):
-    def __init__(self, **options) -> None:
-        super().__init__(**super().enemy['knight'])
 
-class dark_knight(enemy_stats):
-    def __init__(self, **options) -> None:
-        super().__init__(**super().enemy['datk knight'])
+class Hunter(Enemy):
+    def __init__(self, **options):
+        super().__init__('hunter', **options)
 
-class boss_holy_knight(enemy_stats):
-    def __init__(self, **options) -> None:
-        super().__init__(**super().enemy['boss holy knight'])
 
-class esqueletons(enemy_stats):
-    def __init__(self, **options) -> None:
-        super().__init__(**super().enemy['esqueletons'])
+class Goblin(Enemy):
+    def __init__(self, **options):
+        super().__init__('goblin', **options)
 
-class malenia(enemy_stats):
-    def __init__(self, **options) -> None:
-        super().__init__(**super().enemy['malenia'])
+class FallenAngel(Enemy):
+    def __init__(self, **options):
+        super().__init__('fallenangel', **options)
 
-class eldenghost(enemy_stats):
-    def __init__(self, **options) -> None:
-        super().__init__(**super().enemy['eldenghost'])
+class FlameGuy(Enemy):
+    def __init__(self, **options):
+        super().__init__('flameguy', **options)
 
-class demon_king(enemy_stats):
-    def __init__(self, **options) -> None:
-        super().__init__(**super().enemy['demon king'])
-    
-#Aqui va a estar el sistemas de combate
+class Skeletons(Enemy):
+    def __init__(self, **options):
+        super().__init__('skeletons', **options)
 
-class fight_system:
-    def __init__(self, jugador):
-        self.jugador = Human, Demon, Angel
-        
-    def start(self):
-        for i in range():
-            print(f"--- Turno {i+1} ---")
-            for jugador in self.jugador:
-                if jugador.hp <= 0:
-                    continue
-                print(f"!Es tu turno¡")
-                print("¿Qué acción deseas realizar?")
-                print("1. Atacar")
-                print("2. Curar")
-                print("3. Defenderse")
-                opcion = int(input())
-                if opcion == 1:
-                    print("Elige a quién atacar:")
-                    for j, target in enumerate(self.jugador):
-                        if target != jugador and target.hp > 0:
-                            print(f"{j+1}. {target.nombre}")
-                    target = int(input()) - 1
-                    jugador.attack(self.jugador[target])
 
-                elif opcion == 2:
-                    cantidad = random.randint(10, 30, 50, 70, 90, 110, 300)
-                    jugador.curar(cantidad)
+class EldenGhost(Enemy):
+    def __init__(self, **options):
+        super().__init__('eldenghost', **options)
 
-                elif opcion == 3:
-                    jugador.defenderse()
-                    
-                else:
-                    print("invalid option. you lose one turn")
-                    
-            for jugador in self.jugador:
-                jugador.reset_defensa()
-                    
-        print("End of the fight")
-        vivos = [jugador for jugador in self.jugador if jugador.hp > 0]
-        if len(vivos) == 1:
-            print(f"{vivos[0]} you win.")  
+
+class FightSystem:
+    def __init__(self, player_type):
+        player_type = player_type.lower()
+
+        if player_type == 'human':
+            self.player = Human()
+        elif player_type == 'demon':
+            self.player = Demon()
+        elif player_type == 'angel':
+            self.player = Angel()
         else:
-            print("¡it's a tie!")
-                
+            raise ValueError("Invalid player type")
+
+    def start(self, enemies):
+        for i in range(1, 3):  # Número de turnos
+            print(f"--- Turno {i} ---")
+            if self.player.hp <= 0:
+                print("You are defeated.")
+                break
+
+            print("It's your turn!")
+            print("What action do you want to take?")
+            print("1. Attack")
+            print("2. Heal")
+            print("3. Defend")
+            print("-----------------------------------------")
+            option = int(input())
+
+            self.enemies = enemies
+
+            if option == 1:
+                self.player.attack(self.enemies)
+
+            elif option == 2:
+                self.player.heal()
+
+            elif option == 3:
+                self.player.defend()
+
+            else:
+                print("Invalid option. You lose one turn.")
+
+            if enemies.hp > 0:
+                enemies.attack(self.player)
+            else:
+                enemies.reset_defense()
+            
+            
+
+        print("End of the fight.")
+        if self.player.hp <= 0:
+            print("You are defeated.")
+        elif self.enemies == 0:
+            print("You win!")
+        else:
+            print("It's a tie.")
+
 
 '''
 class combat:
@@ -323,7 +285,7 @@ class combat:
                 for i, objetivo in enumerate(self.jugador):
                     if jugador != objetivo and objetivo.vida > 0:
                         print(f"{i+1}. {objetivo}")'''
-                
+
 
 
 
